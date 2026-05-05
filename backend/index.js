a7c6922968ed41842ce2a8c7d3fc80a62d496b42
PabloAlonsoRomero <pabloealonsor@gmail.com>
2026-05-05 01:01:38 -0600

    feat: implementación de middleware de errorHandler y logErrors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const setupSwagger = require('./swagger')
 
+const { logErrors, errorHandler } = require('./middlewares/errorHandler')
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 setupSwagger(app);
+app.use(logErrors)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log('Server running on port ' + port);
